73e81a57c674de740ba94ba2b954a051434e1d66
Chad Dombrova <chadrik@gmail.com>
2012-07-09 22:26:22 -0700

    Added LGPL lic to FindMaya.cmake

--- FindMaya.cmake
+++ FindMaya.cmake
@@ -290,3 +290,20 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Maya DEFAULT_MSG
   MAYA_LIBRARIES MAYA_EXECUTABLE MAYA_INCLUDE_DIR MAYA_LIBRARY_DIR MAYA_VERSION MAYA_PLUGIN_SUFFIX MAYA_USER_DIR
 )
+
+#
+# Copyright 2012, Chad Dombrova
+#
+# vfxcmake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# vfxcmake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with vfxcmake.  If not, see <http://www.gnu.org/licenses/>.
+#
