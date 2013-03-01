7fc49eec98002a99d4e45343a579ef00c0fde602
Nathan Rusch <nathanrusch@gmail.com>
2013-02-28 17:17:40 -0800

    Standardize non-Luma modules on basic style guide

--- cmake/FindTbb.cmake
+++ cmake/FindTbb.cmake
@@ -13,23 +13,23 @@
 
 set(TBB_FOUND FALSE)
 unset(TBBROOT CACHE) # so we can be called multiple times
-find_path(TBBROOT NAMES include/tbb/tbb.h HINTS ${TBB_ROOT} )
+find_path(TBBROOT NAMES include/tbb/tbb.h HINTS ${TBB_ROOT})
 
 if(TBBROOT)
     set(TBB_INCLUDE_DIRS ${TBBROOT}/include)
     set(TBB_LIBRARY_DIRS ${TBBROOT}/lib)
-    
+
     file(READ "${TBB_INCLUDE_DIRS}/tbb/tbb_stddef.h" _TBB_CONTENTS)
     string(REGEX REPLACE ".*#define TBB_VERSION_MAJOR ([0-9]+).*" "\\1" TBB_MAJOR_VERSION "${_TBB_CONTENTS}")
     string(REGEX REPLACE ".*#define TBB_VERSION_MINOR ([0-9]+).*" "\\1" TBB_MINOR_VERSION "${_TBB_CONTENTS}")
     set(TBB_VERSION "${TBB_MAJOR_VERSION}.${TBB_MINOR_VERSION}")
-    
-    set(TBB_FOUND TRUE)
-endif(TBBROOT)
 
-if( Tbb_FIND_REQUIRED AND NOT TBB_FOUND )
+    set(TBB_FOUND TRUE)
+endif()
+
+if(Tbb_FIND_REQUIRED AND NOT TBB_FOUND)
     message(FATAL_ERROR "Could not find tbb")
-endif( Tbb_FIND_REQUIRED AND NOT TBB_FOUND )
+endif()
 
 
 #
